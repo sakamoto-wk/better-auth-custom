570f6d2160d0004c76b4be25ae7ca9e575bbce72
sakamoto-wk <24515562+sakamoto-wk@users.noreply.github.com>
2026-02-03 13:32:29 +0900

    Setup better-auth-custom

---
+++ .github/workflows/scripts/release-update_config-better-auth.cjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const fs = require("node:fs");
+const path = require("node:path");
+
+const version = process.env.VERSION || process.argv[2];
+if (!version) {
+	console.error("VERSION not provided (env VERSION or first arg)");
+	process.exit(1);
+}
+
+const cwd = process.cwd();
+const pkgPath = path.join(cwd, "package.json");
+if (!fs.existsSync(pkgPath)) {
+	console.error("package.json not found at", pkgPath);
+	process.exit(1);
+}
+
+const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
+pkg.name = "@sakamoto-wk/better-auth";
+pkg.version = version;
+pkg.homepage = "https://github.com/sakamoto-wk/better-auth-custom";
+pkg.repository = {
+	type: "git",
+	url: "git+https://github.com/sakamoto-wk/better-auth-custom.git",
+	directory: "packages/better-auth",
+};
+
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
+console.log(`Updated package.json at ${pkgPath}`);
+console.log(`  name: ${pkg.name}`);
+console.log(`  version: ${pkg.version}`);
+
+process.exit(0);
